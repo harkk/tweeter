946693d3d7cc8c8b7823e10be3d2f0c7ca9b43bb
Harkamal Khabra <khabra07@gmail.com>
2019-03-21 21:33:49 +0000

    Fixed styling- corrected issue so only one counter at bottom right now

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
   $("#textarea").on("input", function() {
     var charCount = 140 - $(this).val().length;
     $(".counter").text(140 - $(this).val().length);
-    $(this).siblings(".counter").text(charCount);
     if (charCount < 0) {
       $(".counter").css("color", "red");
     } else {
