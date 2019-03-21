28c9dfc87ca5e120d3255cb361994b7a23567d5f
Harkamal Khabra <khabra07@gmail.com>
2019-03-21 19:34:01 +0000

    Implemented renderTweets function- cleaned up index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,9 +5,8 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-$(document).ready(function () {
-  console.log("Loaded");
-  const tweetData = {
+const data = [
+  {
     "user": {
       "name": "Newton",
       "avatars": {
@@ -21,11 +20,50 @@ $(document).ready(function () {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
   }
+];
 
+function renderTweets(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  tweets.forEach(tweet => {
+    let renderedTweet = createTweetElement(tweet);
+    $('.container').append(renderedTweet);
+  })
+}
 
-  function createTweetElement(tweet) {
-    return `<section class ="tweet-container">
+function createTweetElement(tweet) {
+  return `<section class ="tweet-container">
       <article class="tweet">
       <header>
         <img src="${tweet.user.avatars.small}" />
@@ -40,10 +78,4 @@ $(document).ready(function () {
     </section>`
   }
 
-
-  var $tweet = createTweetElement(tweetData);
-
-  // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-  $('.container').append($tweet);
-})
-
+renderTweets(data);
